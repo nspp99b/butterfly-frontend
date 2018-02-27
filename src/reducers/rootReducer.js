5168ebfa0dfbd45237d12b6cf4a23d0e0a2b22d7
nspp99b <chromepanthers@gmail.com>
2018-02-27 14:18:38 -0500

    implemented showflapeffects toggle

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -11,7 +11,7 @@ function flapsReducer(state = { flaps: [] }, action) {
     case 'SHOW_EFFECTS':	
       const f = state.flaps.find(flap => flap.id === action.flapId)
       const i = state.flaps.indexOf(f)
-      f.showFlapEffects = true
+      f.showFlapEffects = !f.showFlapEffects
       return { flaps: [...state.flaps.slice(0,i), f, ...state.flaps.slice(i+1)] }
       
     case 'FETCH_FLAPS':
