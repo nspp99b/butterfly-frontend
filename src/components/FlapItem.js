0d71ca0b3700646522d06dce0da1a4e9c6e5a3c2
nspp99b <chromepanthers@gmail.com>
2018-02-27 15:37:56 -0500

    preliminary create effect styling added to flapItem

--- src/components/FlapItem.js
+++ src/components/FlapItem.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import FlapList from './FlapList'
+import FlapCreate from './FlapCreate'
 
-const FlapItem = (props) => {
+class FlapItem extends React.Component {
   
-    console.log('FlapItem rendered')
-    console.log(props)
+  state = {
+    imageSrc: "butterfly_logo_inverted_25px.png"
+  }
+  
+  handleMouseEnter = () => {
+    this.setState({ imageSrc: "butterfly_logo_purple_25px.png"})
+  }
+  
+  handleMouseLeave = () => {
+    this.setState({ imageSrc: "butterfly_logo_inverted_25px.png"})
+  }
+  
+  render() {
     return (
       <div className="flapItem">
-        <div className="flapItem-user">{props.flap.user.name}</div>
-        <div>{props.flap.content}</div>
-        <div className="flapItem-fx" onClick={() => props.showEffects(props.flap.id)}>FX: {props.flap.fx_count}</div>
-        { props.flap.showFlapEffects === true && < FlapList flaps={props.flap.effects} showEffects={props.showEffects}/> }
+        <div className="flapItem-user">{this.props.flap.user.name}</div>
+        <div>{this.props.flap.content}</div>
+        <img className="flapItem-img" src={this.state.imageSrc} alt="create effect" onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave}></img>
+        <div className="flapItem-fx" onClick={() => this.props.showEffects(this.props.flap.id)}>FX: {this.props.flap.fx_count}</div>
+        
+        { this.props.flap.showFlapEffects === true && < FlapList flaps={this.props.flap.effects} showEffects={this.props.showEffects}/> }
       </div>
     )
+  }
+
 
 }
 
