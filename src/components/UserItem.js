5ea54239155e2e23345d93ac61317c29520fa1dc
nspp99b <chromepanthers@gmail.com>
2018-03-01 12:47:22 -0500

    styling for userItem and Users navlink

--- src/components/UserItem.js
+++ src/components/UserItem.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const UserItem = (props) => {
   return (
-    <div className="userItem">{props.user.name}</div>
+    <div className="userItem">
+      <div className="userItem-name">{props.user.name}</div>
+    </div>
   )
 }
 
