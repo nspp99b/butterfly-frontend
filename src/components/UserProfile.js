7c6d5139b08807c32b0ecf43169f3cddc9f87cb3
nspp99b <chromepanthers@gmail.com>
2018-03-05 13:58:54 -0500

    refactored userProfile so followbutton is its own component, conditionally rendered

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -5,14 +5,20 @@ const UserProfile = (props) => {
   console.log('UserProfile rendered')
   console.log(props)
 
+  const FollowButton = (props) => {
+    console.log('display Button fired')
+    return (
+      props.user.followers.includes(props.currentUser.id) ?
+      <button className="userProfile-followbuttons" onClick={props.unfollow}>unfollow</button>
+      :
+      <button className="userProfile-followbuttons" onClick={() => props.follow(props.currentUser.id, props.user.id)}>follow</button>
+    )
+  }
+
   return (
     <div className="userProfile">
       <span>{props.user.name}</span>
-        { props.user.followers.includes(props.currentUser.id) ?
-          <button className="userProfile-followbuttons" onClick={props.unfollow}>unfollow</button>
-          :
-          <button className="userProfile-followbuttons" onClick={() => props.follow(props.currentUser.id, props.user.id)}>follow</button>
-        }
+        { props.currentUser && <FollowButton user={props.user} currentUser={props.currentUser} follow={props.follow}/>}
     </div>
 
   )
