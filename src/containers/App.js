9577185f2a637a9bf387168947877bd3c0305b82
nspp99b <chromepanthers@gmail.com>
2018-03-06 13:19:04 -0500

    removed border from flap create, added new route for profile

--- src/containers/App.js
+++ src/containers/App.js
@@ -30,6 +30,7 @@ class App extends React.Component {
         <Route exact path="/login" render={() => (localStorage.getItem('token') ? (<Redirect to="/main"/>) : (<Login/>))}/>
         <Route exact path="/signup" render={() => (this.props.isLoggedIn ? (<Redirect to="/main"/>) : (<Signup/>))}/>
         <Route exact path="/main" render={() => (localStorage.getItem('token') ? (<MainContainer currentUser={this.props.currentUser}/>) : (<Redirect to="/login"/>))}/>
+        <Route exact path="/profile" render={(props) => (localStorage.getItem('token') ? (<ProfileContainer userToFetch={this.props.currentUser.id}/>) : (<Redirect to="/login"/>))}/>
         <Route exact path="/users/:userid" render={(props) => (localStorage.getItem('token') ? (<ProfileContainer userToFetch={props.match.params.userid}/>) : (<Redirect to="/login"/>))}/>
         <Route exact path="/users" render={() => (localStorage.getItem('token') ? (<UsersContainer/>) : (<Redirect to="/login"/>))}/>
 
