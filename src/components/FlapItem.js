e805d1abf330c5568b7c3c92a20422288b3d9059
nspp99b <chromepanthers@gmail.com>
2018-03-06 17:46:53 -0500

    added table column display to flapItems

--- src/components/FlapItem.js
+++ src/components/FlapItem.js
@@ -32,23 +32,27 @@ class FlapItem extends React.Component {
     console.log(this.props)
     return (
       <div className="flapItem">
-        <img className="userProfile-img" src={this.props.flap.user.image} alt="" width="80px" height="80px"/>
-        <Link to={`/users/${this.props.flap.user.id}`}>{this.props.flap.user.name}</Link>
+        <div className="flapItem-left">
+          <img className="userProfile-img" src={this.props.flap.user.image} alt="" width="80px" height="80px"/>
+        </div>
+        <div className="flapItem-right">
+          <Link to={`/users/${this.props.flap.user.id}`}>{this.props.flap.user.name}</Link>
 
-        { this.props.flap.user.id === this.props.currentUser.id &&
-        <span className="flapItem-delete" onClick={() => this.props.updateFlap(this.props.flap.id)}>x</span> }
-        <span className="flapItem-timeago">
-          {this.props.flap.created_at}
-        </span>
-        <div>{this.props.flap.content}</div>
-        { this.props.flap.user.name !== "--" &&
-        <div>
-          <img className="flapItem-img" src={this.state.imageSrc} alt="" onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave} onClick={this.handleFlapCreate}/>
-          { this.state.showFlapCreate === true && < FlapCreate currentUser={this.props.currentUser} parent={this.props.flap.id}/> }
-        </div> }
-        <div className="flapItem-fx" onClick={() => this.handleShowFlapEffects()}>FX: {this.props.flap.fx_count}</div>
+          { this.props.flap.user.id === this.props.currentUser.id &&
+          <span className="flapItem-delete" onClick={() => this.props.updateFlap(this.props.flap.id)}>x</span> }
+          <span className="flapItem-timeago">
+            {this.props.flap.created_at}
+          </span>
+          <div>{this.props.flap.content}</div>
+          { this.props.flap.user.name !== "--" &&
+          <div>
+            <img className="flapItem-img" src={this.state.imageSrc} alt="" onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave} onClick={this.handleFlapCreate}/>
+            { this.state.showFlapCreate === true && < FlapCreate currentUser={this.props.currentUser} parent={this.props.flap.id}/> }
+          </div> }
+          <div className="flapItem-fx" onClick={() => this.handleShowFlapEffects()}>FX: {this.props.flap.fx_count}</div>
 
-        { this.state.showFlapEffects === true && < FlapList flaps={this.props.flap.effects} currentUser={this.props.currentUser}/> }
+          { this.state.showFlapEffects === true && < FlapList flaps={this.props.flap.effects} currentUser={this.props.currentUser}/> }
+        </div>
       </div>
     )
   }
