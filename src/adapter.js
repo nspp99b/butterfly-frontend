57f4ec6237fbb47daf884e2c48084f64a52aeaa0
nspp99b <chromepanthers@gmail.com>
2018-05-23 14:02:43 -0400

    delint adapter

--- src/adapter.js
+++ src/adapter.js
@@ -4,15 +4,15 @@ const headers = () => {
   return {'Content-Type': 'application/json',
   Accepts: 'application/json',
   Authorization: localStorage.getItem('token') }
-}
+};
 
 const signup_headers = {
   'Content-Type': 'application/json',
   Accepts: 'application/json'
-}
+};
 
-const URL_ROOT = 'http://localhost:3001'
-const API_ROOT = `${URL_ROOT}/api/v1`
+const URL_ROOT = 'http://localhost:3001';
+const API_ROOT = `${URL_ROOT}/api/v1`;
 
 // auth fetches
 
@@ -21,8 +21,8 @@ const signup = (signupBody) => {
     method: 'POST',
     headers: signup_headers,
     body: JSON.stringify({user: signupBody})
-  }).then(res => res.json())
-}
+  }).then(res => res.json());
+};
 
 const login = (email, password) => {
   return fetch(`${URL_ROOT}/login`, {
@@ -30,13 +30,13 @@ const login = (email, password) => {
     headers: headers(),
     body: JSON.stringify({ email, password })
   }).then(res => res.json());
-}
+};
 
 const getLoggedInUser = () => {
   return fetch(`${URL_ROOT}/current_user`, {
     headers: headers()
   }).then(res => res.json())
-}
+};
 
 // users
 
@@ -44,13 +44,13 @@ const getUsers = () => {
   return fetch(`${API_ROOT}/users`, {
     headers: headers()
   }).then(res => res.json());
-}
+};
 
 const getUser = (uid) => {
   return fetch(`${API_ROOT}/users/${uid}`, {
     headers: headers()
   }).then(res => res.json());
-}
+};
 
 const patchUser = (uid, patchUserBody) => {
   return fetch(`${API_ROOT}/users/${uid}`, {
@@ -58,7 +58,7 @@ const patchUser = (uid, patchUserBody) => {
     headers: headers(),
     body: JSON.stringify({ user: patchUserBody })
   }).then(res => res.json());
-}
+};
 
 // follows
 
@@ -68,7 +68,7 @@ const postFollow = (follower_id, followed_id) => {
     headers: headers(),
     body: JSON.stringify({ follower_id, followed_id })
   }).then(res => res.json());
-}
+};
 
 const postUnfollow = (follower_id, followed_id) => {
   return fetch(`${API_ROOT}/unfollow`, {
@@ -76,19 +76,19 @@ const postUnfollow = (follower_id, followed_id) => {
     headers: headers(),
     body: JSON.stringify({ follower_id, followed_id })
   }).then(res => res.json());
-}
+};
 
 const getFollowing = (uid) => {
   return fetch(`${API_ROOT}/users/${uid}/following`, {
     headers: headers()
   }).then(res => res.json());
-}
+};
 
 const getFollowers = (uid) => {
   return fetch(`${API_ROOT}/users/${uid}/followers`, {
     headers: headers()
   }).then(res => res.json());
-}
+};
 
 // flaps
 
@@ -96,13 +96,13 @@ const getFlaps = () => {
   return fetch(`${API_ROOT}/flaps`, {
     headers: headers()
   }).then(res => res.json());
-}
+};
 
 const getFlap = (fid) => {
   return fetch(`${API_ROOT}/flaps/${fid}`, {
     headers: headers()
   }).then(res => res.json());
-}
+};
 
 const postFlap = (content, user_id, parent) => {
   return fetch(`${API_ROOT}/flaps`, {
@@ -110,14 +110,14 @@ const postFlap = (content, user_id, parent) => {
     headers: headers(),
     body: JSON.stringify({ content, user_id, parent })
   }).then(res => res.json());
-}
+};
 
 const patchFlap = (fid) => {
   return fetch(`${API_ROOT}/flaps/${fid}`, {
     method: 'PATCH',
     headers: headers()
   }).then(res => res.json());
-}
+};
 
 export default {
   auth: {
@@ -142,4 +142,4 @@ export default {
     getFollowing,
     getFollowers
   }
-}
+};
