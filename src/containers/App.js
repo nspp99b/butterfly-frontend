8cfbb3724cfe8892279fb3334b45ee9b0af00f21
nspp99b <chromepanthers@gmail.com>
2018-05-23 14:39:18 -0400

    delint app container

--- src/containers/App.js
+++ src/containers/App.js
@@ -15,6 +15,8 @@ import FollowersContainer from './FollowersContainer';
 
 class App extends React.Component {
 
+  // if user never logged out, let them in
+
   componentDidMount() {
     const token = localStorage.getItem('token');
     if (token) {
@@ -22,33 +24,33 @@ class App extends React.Component {
     } else {
       console.log('No token found');
     }
-  }
+  };
 
   render() {
-    console.log('app rendered')
+    console.log('app rendered');
     return (
       <div className="app-wrapper">
         <NavBar currentUser={this.props.currentUser} logout={this.props.logoutUser}/>
         <Switch>
-        <Route exact path="/login" render={() => (localStorage.getItem('token') ? (<Redirect to="/main"/>) : (<Login/>))}/>
-        <Route exact path="/signup" render={() => (this.props.isLoggedIn ? (<Redirect to="/main"/>) : (<Signup currentUser={this.props.currentUser}/>))}/>
-        <Route exact path="/main" render={() => (localStorage.getItem('token') ? (<MainContainer currentUser={this.props.currentUser}/>) : (<Redirect to="/login"/>))}/>
-        <Route exact path="/profile" render={(props) => (localStorage.getItem('token') ? (<ProfileContainer userToFetch={this.props.currentUser.id}/>) : (<Redirect to="/login"/>))}/>
-        <Route exact path="/users/:userid/following" render={(props) => (localStorage.getItem('token') ? (<FollowingContainer userToFetch={props.match.params.userid}/>) : (<Redirect to="/login"/>))}/>
-        <Route exact path="/users/:userid/followers" render={(props) => (localStorage.getItem('token') ? (<FollowersContainer userToFetch={props.match.params.userid}/>) : (<Redirect to="/login"/>))}/>
-        <Route exact path="/users/:userid" render={(props) => (localStorage.getItem('token') ? (<ProfileContainer userToFetch={props.match.params.userid}/>) : (<Redirect to="/login"/>))}/>
-        <Route exact path="/users" render={() => (localStorage.getItem('token') ? (<UsersContainer/>) : (<Redirect to="/login"/>))}/>
+          <Route exact path="/login" render={() => (localStorage.getItem('token') ? (<Redirect to="/main"/>) : (<Login/>))}/>
+          <Route exact path="/signup" render={() => (this.props.isLoggedIn ? (<Redirect to="/main"/>) : (<Signup currentUser={this.props.currentUser}/>))}/>
+          <Route exact path="/main" render={() => (localStorage.getItem('token') ? (<MainContainer currentUser={this.props.currentUser}/>) : (<Redirect to="/login"/>))}/>
+          <Route exact path="/profile" render={(props) => (localStorage.getItem('token') ? (<ProfileContainer userToFetch={this.props.currentUser.id}/>) : (<Redirect to="/login"/>))}/>
+          <Route exact path="/users/:userid/following" render={(props) => (localStorage.getItem('token') ? (<FollowingContainer userToFetch={props.match.params.userid}/>) : (<Redirect to="/login"/>))}/>
+          <Route exact path="/users/:userid/followers" render={(props) => (localStorage.getItem('token') ? (<FollowersContainer userToFetch={props.match.params.userid}/>) : (<Redirect to="/login"/>))}/>
+          <Route exact path="/users/:userid" render={(props) => (localStorage.getItem('token') ? (<ProfileContainer userToFetch={props.match.params.userid}/>) : (<Redirect to="/login"/>))}/>
+          <Route exact path="/users" render={() => (localStorage.getItem('token') ? (<UsersContainer/>) : (<Redirect to="/login"/>))}/>
         </Switch>
       </div>
     );
-  }
-}
+  };
+};
 
 const mapStateToProps = (state) => {
   return {
     currentUser: state.usersReducer.currentUser,
     isLoggedIn: state.usersReducer.isLoggedIn,
   }
-}
+};
 
 export default withRouter(connect(mapStateToProps, actions)(App));
