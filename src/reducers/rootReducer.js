a8cf3a11c34c788d7299fda45a2e8a22908dca3d
nspp99b <chromepanthers@gmail.com>
2018-05-23 14:17:40 -0400

    delint rootReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -5,42 +5,51 @@ export const rootReducer = combineReducers({
   flapsReducer
 });
 
-function usersReducer(state = { currentUser: {id: 0, name: "", email: "", image: "", following: [], followers: []}, isLoggedIn: false, users: [], selectedUser: {id: 0, name: "", email: "", image: "", following: [], followers: []} }, action) {
+function usersReducer(state = {
+  currentUser: {
+    id: 0, name: "", email: "", image: "", following: [], followers: []
+  },
+  isLoggedIn: false,
+  users: [],
+  selectedUser: {
+    id: 0, name: "", email: "", image: "", following: [], followers: []
+  }
+}, action) {
   switch (action.type) {
 
     case 'SET_CURRENT_USER':
-      console.log(`set current user payload: ${action.payload}`)
+      console.log(`set current user payload: ${action.payload}`);
       if (action.payload.token != null) {
-        localStorage.setItem('token', action.payload.token)
-      }
-      return {...state, currentUser: action.payload.user, isLoggedIn: true }
+        localStorage.setItem('token', action.payload.token);
+      };
+      return { ...state, currentUser: action.payload.user, isLoggedIn: true };
 
     case 'REFRESH_CURRENT_USER':
-      return {...state, currentUser: action.payload}
+      return { ...state, currentUser: action.payload };
 
     case 'LOGOUT_USER':
       localStorage.removeItem('token')
-      return { ...state, currentUser: {id: 0, name: "", email: "", image: "", following: [], followers: []}, isLoggedIn: false }
+      return { ...state, currentUser: {id: 0, name: "", email: "", image: "", following: [], followers: [] }, isLoggedIn: false};
 
     case 'FETCH_USERS':
-      return { ...state, users: action.payload }
+      return { ...state, users: action.payload };
 
     case 'FETCH_USER':
-      return { ...state, selectedUser: action.payload}
+      return { ...state, selectedUser: action.payload };
 
     default:
-      return state
-  }
-}
+      return state;
+  };
+};
 
 function flapsReducer(state = { flaps: [] }, action) {
   switch (action.type) {
 
     case 'FETCH_FLAPS':
- 	    return { flaps: action.payload }
+ 	    return { flaps: action.payload };
 
     case 'UPDATE_FLAP':
-      return { flaps: action.payload }
+      return { flaps: action.payload };
 
     default:
       return state;
