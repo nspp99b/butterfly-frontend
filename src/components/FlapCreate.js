7a28a3fcb3a44845500b6973c705d934c57b66ef
nspp99b <chromepanthers@gmail.com>
2018-05-23 14:57:44 -0400

    delints flapcreate

--- src/components/FlapCreate.js
+++ src/components/FlapCreate.js
@@ -9,32 +9,35 @@ class FlapCreate extends React.Component {
   }
 
   handleFlapCreate = (e) => {
-    e.preventDefault()
+    e.preventDefault();
+
+    // if inside an existing flap, hide yourself
+
     if (this.props.parent > 0) {
-      this.props.handleShowFlapCreate()
-    }
-    this.props.addFlap(this.state.content, this.props.currentUser.id, this.props.parent)
+      this.props.handleShowFlapCreate();
+    };
+    this.props.addFlap(this.state.content, this.props.currentUser.id, this.props.parent);
     this.setState({
       content: ''
-    })
-  }
+    });
+  };
 
   onInputChange = (e) => {
     this.setState({
       content: e.target.value
-    })
-  }
+    });
+  };
 
   render() {
-    console.log('FlapCreate rendered')
-    console.log(this.props)
+    console.log('FlapCreate rendered');
+    console.log(this.props);
     return (
       <form className="flap-create" onSubmit={this.handleFlapCreate}>
         <textarea className="flap-create-textarea" placeholder="what did you do now..." value={this.state.content} name="content" onChange={this.onInputChange}></textarea>
         <input className="flap-create-submit-button" type="submit" value="Flap!" />
       </form>
-    )
-  }
-}
+    );
+  };
+};
 
-export default connect(null, { addFlap })(FlapCreate)
+export default connect(null, { addFlap })(FlapCreate);
